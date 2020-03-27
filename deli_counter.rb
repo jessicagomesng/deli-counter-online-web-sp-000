809eb5452d1392535b34350e3f3e7dc3c23fcc8d
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-27 15:55:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 katz_deli = []
 
-def line
+def line(katz_deli)
   if katz_deli.length == 0
     "The line is currently empty."
 
@@ -9,4 +9,5 @@ def line
     a = katz_deli.each_with_index do |person, index|
       print "#{index + 1}. #{person}"
     end
+    a.join
 end
