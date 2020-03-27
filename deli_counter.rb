83f04277211299bb7f2044aba8b21c54ddc6f4a7
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-27 15:46:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,11 +5,7 @@ def line
   if katz_deli.length == 0
     "The line is currently empty."
 
-  counter = 0
-  number_of_customers = katz_deli.length 
   else
-    "The line is currently #{index + 1}. #{person}
-
     katz_deli.each_with_index do |person, index|
       print "The line is currently #{index + 1}. #{person}"
     end
