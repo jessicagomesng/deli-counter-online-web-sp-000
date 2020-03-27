52efb2d8dff984eb19fee4eea9c2a065e55ef91d
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-27 16:18:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,7 +24,8 @@ def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
 
-  else 
-  puts "Currently serving #{katz_deli[0]}."
-  katz_deli.shift
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+  end 
 end
