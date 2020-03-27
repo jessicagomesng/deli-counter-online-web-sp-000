fb5f740b5cd268832f8b717fae631e64915dc067
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-27 16:13:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,6 +15,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
+  katz_deli << name 
   position = katz_deli.index(name)
   puts "Welcome, #{name}. You are number #{position.to_i + 1} in line."
 end
