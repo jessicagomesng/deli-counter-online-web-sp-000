9cb53f3a53603df8737e839d9c4584bf0ee767de
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-27 15:53:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line
 
   else
     "The line is currently: " +
-    a = katz_deli.map.each_with_index do |person, index|
+    a = katz_deli.each_with_index do |person, index|
       print "#{index + 1}. #{person}"
-    end 
+    end
 end
